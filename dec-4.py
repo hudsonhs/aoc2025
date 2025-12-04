439572ed3b7f8987fefbffa524687e7739abd3a1
hudsonhs <hharriman.smith@gmail.com>
2025-12-04 18:04:59 -0500

    Some optimizations

--- dec-4.py
+++ dec-4.py
@@ -1,6 +1,6 @@
 def getNumRemovableRolls():
     rolls = serializeRolls('inputs/dec-4')
-    prevRemovableRolls = []
+    prevRemovableRolls = set()
     res = 0
     currRemovableRolls = getAccessibleRolls(rolls)
     while prevRemovableRolls != currRemovableRolls:
@@ -12,35 +12,34 @@ def getNumRemovableRolls():
 
 def serializeRolls(path: str) -> list:
     rolls = []
-    file = open(path)
-    for line in file:
-        row = []
-        for char in line.strip():
-            row.append(char)
-        rolls.append(row.copy())
+    with open(path) as file:
+        for line in file:
+            rolls.append(list(line.strip()))
     return rolls
 
-def getAccessibleRolls(rolls):
-    res = []
+def getAccessibleRolls(rolls: list) -> set:
+    res = set()
     for y in range(len(rolls)):
         for x in range(len(rolls[0])):
             if rolls[y][x] == '@' and isRollAccessible(y, x, rolls):
-                res.append((y, x))
+                res.add((y, x))
     return res
 
 def isRollAccessible(y: int, x: int, rolls: list) -> bool:
     adjRolls = 0
     for dy in (-1, 0, 1):
         for dx in (-1, 0, 1):
+            if dy == 0 and dx == 0:
+                continue
             checkY = y + dy
             checkX = x + dx
-            if (not (dy == 0 and dx == 0)) and 0 <= checkY < len(rolls) and 0 <= checkX < len(rolls[0]) and rolls[checkY][checkX] == '@':
+            if 0 <= checkY < len(rolls) and 0 <= checkX < len(rolls[0]) and rolls[checkY][checkX] == '@':
                 adjRolls += 1
                 if adjRolls > 3:
                     return False
     return True
 
-def removeRolls(rolls: list, rollsToRemove: list) -> None:
+def removeRolls(rolls: list, rollsToRemove: set) -> None:
     for y, x in rollsToRemove:
         rolls[y][x] = '.'
 
