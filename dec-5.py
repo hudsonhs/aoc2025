fdc423198cab02c53d09ef6ce5b7f2da8a023a65
hudsonhs <hharriman.smith@gmail.com>
2025-12-05 08:22:17 -0500

    Create util function for getting today's input file with test boolean toggle input

--- dec-5.py
+++ dec-5.py
@@ -1,32 +1,36 @@
-def getNumIngredientsInRanges(path: str):
+from utils import getTodayInputFile
+
+USE_TEST_INPUT = False
+
+def getNumIngredientsInRanges():
     res = 0
-    ranges = getRanges(path)
-    ingredients = getIngredients(path)
+    ranges = getRanges()
+    ingredients = getIngredients()
     for ingredient in ingredients:
         for range in mergeIntervals(ranges):
             if isInInterval(ingredient, range):
                 res += 1
     return res
 
-def getNumFreshIngredients(path: str):
+def getNumFreshIngredients():
     res = 0
-    intervals = mergeIntervals(getRanges(path))
+    intervals = mergeIntervals(getRanges())
     for interval in intervals:
         res += (interval[1] - interval[0]) + 1
     return res
 
-def getRanges(path: str) -> list:
+def getRanges() -> list:
     ranges = []
-    with open(path) as file:
+    with getTodayInputFile(USE_TEST_INPUT) as file:
         for line in file:
             if line == '\n':
                 break
             ranges.append([int(line.split('-')[0]), int(line.split('-')[1])])
     return ranges
 
-def getIngredients(path: str) -> list:
+def getIngredients() -> list:
     ingredients = []
-    with open(path) as file:
+    with getTodayInputFile(USE_TEST_INPUT) as file:
         startCollectingIngredients = False
         for line in file:
             if startCollectingIngredients:
@@ -52,9 +56,7 @@ def mergeIntervals(intervals: list) -> list:
 def isInInterval(num: int, interval: list):
     return interval[0] <= num <= interval[1]
 
-# print(getNumIngredientsInRanges('inputs/dec-5-test'))
-print(getNumIngredientsInRanges('inputs/dec-5'))
-# print(getNumFreshIngredients('inputs/dec-5-test'))
-print(getNumFreshIngredients('inputs/dec-5'))
+print(getNumIngredientsInRanges())
+print(getNumFreshIngredients())
 
 # Make your own little edge cases in the smaller testing input
--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from time import time
+from pathlib import Path
+from datetime import date
 
 def timer(func):
     # This function shows the execution time of 
@@ -10,3 +12,7 @@ def timer(func):
         print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
         return result
     return wrap_func
+
+def getTodayInputFile(test: bool):
+    testStr = '-test' if test else ''
+    return Path(f'./inputs/dec-{date.today().day}{testStr}').open()
