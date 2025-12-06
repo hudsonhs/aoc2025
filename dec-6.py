bc53d75ec55b70f4a5b7dd3a9927d0ad80702e94
hudsonhs <hharriman.smith@gmail.com>
2025-12-06 08:27:42 -0500

    Day 6 entry

---
+++ dec-6.py
@@ -0,0 +1,75 @@
+from utils import getTodayInputFile
+import math
+
+USE_TEST_INPUT = False
+
+def getAnswersSum() -> int:
+    inputMatrix = transformInput(getSplitInput())
+    return sum([getRowAnswer(row) for row in inputMatrix])
+
+def getAnswersSum2() -> int:
+    inputMatrix = getTransformedInput2()
+    return sum([getRowAnswer(row) for row in inputMatrix])
+
+
+def getSplitInput() -> list:
+    res = []
+    with getTodayInputFile(USE_TEST_INPUT) as file:
+        for line in file:
+            res.append(line.split())
+    return res
+
+def transformInput(matrix: list) -> list:
+    res = []
+    for i in range(len(matrix[0])):
+        row = []
+        for j in range(len(matrix)):
+            char = matrix[j][i]
+            if char.isnumeric():
+                row.append(int(char))
+            else:
+                row.append(char)
+        res.append(row)
+    return res
+
+def getRowAnswer(row: list) -> int:
+    if row[-1] == '+':
+        func = sum
+    elif row[-1] == '*':
+        func = math.prod
+    return func(row[:-1])
+
+def getTransformedInput2():
+    matrix = []
+    row = []
+    operator = ''
+    foundOperator = False
+    with getTodayInputFile(USE_TEST_INPUT) as file:
+        lines = file.readlines()
+        lineLen = len(lines[0])
+        for col in range(lineLen - 2, -1, -1):
+            number = ''
+            for line in lines:
+                char = line[col]
+                if char.isspace():
+                    continue
+                if not char.isnumeric():
+                    foundOperator = True
+                    operator = char
+                    break
+                number += char
+            if foundOperator:
+                row.append(int(number))
+                row.append(operator)
+                matrix.append(row)
+                row = []
+                operator = ''
+                foundOperator = False
+                # skip the next iteration, continue might not be enough here. MIght need to decrease col
+            else:
+                if number:
+                    row.append(int(number))
+    return matrix
+
+print(getAnswersSum())
+print(getAnswersSum2())
